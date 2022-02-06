b38c9ee6ee2e92b730b52ca9d7221b24e85b9fab
AnaOddles <ASanchezSa2@my.gcu.edu>
2022-02-06 00:48:46 -0700

    Add User Service and Model

---
+++ SofanaGPSApi/Models/User.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using MongoDB.Bson;
+using MongoDB.Bson.Serialization.Attributes;
+
+namespace SofanaGPSApi.Models
+{
+    public class User
+    {
+        [BsonId] //Donoates primary key
+        [BsonRepresentation(BsonType.ObjectId)] // Converts to string for parameters
+        
+        //Properties & getter & setters
+        public string Id { get; set; } 
+
+        [BsonElement("username")]
+        public string username { get; set; }
+
+        [BsonElement("password")]
+        public string password { get; set; }
+    }
+}
