db81941d9f27a980da09faab54ca5ddd0bf42ad4
AnaOddles <ASanchezSa2@my.gcu.edu>
2022-02-06 00:46:22 -0700

    Add BasicAuth Filter Attribute

---
+++ SofanaGPSApi/AuthAttribute/BasicAuthAttribute.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace SofanaGPSApi.AuthAttribute
+{
+    //Registering the BasicAuth Attribute as a filter attribute
+    [AttributeUsage(AttributeTargets.Class | AttributeTargets.Method)] // Can be used on class and method level
+    public class BasicAuthAttribute : TypeFilterAttribute
+    {
+        public BasicAuthAttribute()
+            : base(typeof(BasicAuthorizeFilter)) {}
+    }
+}
